8c5299200b2cd0ec8c0e158e35e84a572fe96b10
younglee <young.lee@ucb.edu.bo>
2023-02-23 19:36:11 -0400

    saludador con edad

--- src/presenter.js
+++ src/presenter.js
@@ -4,9 +4,10 @@ const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 const genero = document.querySelector("#comboGenero");
+const edad = document.querySelector("#edad");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<h3>" + saludo(nombre.value, genero.options[genero.selectedIndex].value) + "</h3>";
+  div.innerHTML = "<h3>" + saludo(nombre.value, genero.options[genero.selectedIndex].value,edad.value) + "</h3>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,8 +1,19 @@
-function saludo(nombre, genero) {
-  saludo = "Hola " + nombre;
-  if (genero != "vacio") {
-    saludo= "Hola " + genero + " " + nombre;
+function saludo(nombre, genero, edad) {
+  saludo =""
+  if(genero != "vacio" && edad < 29){
+    saludo+= genero;
   }
+  if(genero == "vacio" && edad > 29){
+    saludo+= "Sr/Sra.";
+  }
+  if(genero != "vacio" && edad > 29){
+    if (genero == "joven") {
+      saludo+= "Sr.";
+    }else{
+      saludo+= "Sra.";
+    }
+  }
+  saludo="Hola " + saludo + " " + nombre;
   return saludo;
 }
 
