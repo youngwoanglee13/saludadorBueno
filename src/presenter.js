eef4b712361084aa2bc5b90facaa3e862e288bfe
younglee <young.lee@ucb.edu.bo>
2023-02-23 20:13:46 -0400

    saludar segun el idioma

--- src/presenter.js
+++ src/presenter.js
@@ -5,9 +5,10 @@ const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 const genero = document.querySelector("#comboGenero");
 const edad = document.querySelector("#edad");
+const idioma = document.querySelector("#comboIdioma");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<h3>" + saludo(nombre.value, genero.options[genero.selectedIndex].value,edad.value) + "</h3>";
+  div.innerHTML = "<h3>" + saludo(nombre.value, genero.options[genero.selectedIndex].value,edad.value,idioma.options[idioma.selectedIndex].value) + "</h3>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,11 +1,7 @@
-function saludo(nombre, genero, edad) {
+function saludoES(nombre, genero, edad) {
   saludo =""
-  if(genero != "vacio" && edad < 30){
-    saludo+= genero;
-  }
-  if(genero == "vacio" && edad > 29){
-    saludo+= "Sr/Sra.";
-  }
+  if(genero != "vacio" && edad < 30)saludo+= genero;
+  if(genero == "vacio" && edad > 29)saludo+= "Sr/Sra.";
   if(genero != "vacio" && edad > 29){
     if (genero == "joven") {
       saludo+= "Sr.";
@@ -14,18 +10,47 @@ function saludo(nombre, genero, edad) {
     }
   }
   let fechaActual = new Date();
-  if(fechaActual.getHours()<12){
-    saludo="Buenos dias " + saludo + " " + nombre;
-  }
+  if(fechaActual.getHours()<12) saludo="Buenos dias " + saludo + " " + nombre;
   if(fechaActual.getHours()>=19){
     saludo="Buenas noches " + saludo + " " + nombre;
   }else{
     saludo="Buenas tardes " + saludo + " " + nombre;
   }
-
- 
-
   return saludo;
 }
+function saludoEN(nombre, genero, edad) {
+  saludo =""
+  if(genero != "vacio" && edad < 30){
+    if (genero == "señorita") {
+      saludo+= "Miss.";
+    }
+  }
+  if(genero == "vacio" && edad > 29)saludo+= "Mr/Mrs.";
+  if(genero != "vacio" && edad > 29){
+    if (genero == "joven") {
+      saludo+= "Mr.";
+    }else{
+      saludo+= "Mrs.";
+    }
+  }
+  let fechaActual = new Date();
+  if(fechaActual.getHours()<12) saludo="Good morning " + saludo + " " + nombre;
+  if(fechaActual.getHours()>=19){
+    saludo="Goog evening " + saludo + " " + nombre;
+  }else{
+    saludo="Good afternoon " + saludo + " " + nombre;
+  }
+  return saludo;
+}
+
+function saludo(nombre, genero, edad, idioma) {
+  if(idioma == "ES"){
+    return saludoES(nombre, genero, edad);
+  }else{
+    return saludoEN(nombre, genero, edad);
+  }
+}
+
+
 
 export default saludo;
